58a2f5dbdd90e9e4b1850a09b8f416f6673da8d9
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-06-26 11:37:49 +0200

    Connect quiz brain to interface

--- ui.py
+++ ui.py
@@ -1,11 +1,14 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 FONT = ('Arial', 20, 'normal')
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title('Quiz-API')
         self.window.minsize(width=500, height=700)
@@ -24,9 +27,10 @@ class QuizInterface:
                              bg='white')
         self.question_text = self.canvas.create_text(
             225, 200,
+            width=430,
             text='Question',
             fill=THEME_COLOR,
-            font =FONT
+            font=FONT
         )
         self.canvas.grid(row=1, column=0, columnspan=2,
                          padx=25, pady=25)
@@ -36,7 +40,7 @@ class QuizInterface:
 
         self.true_button = Button(
             image=true_img,
-            highlightthickness= 0,
+            highlightthickness=0,
             borderwidth=0,
         )
         self.true_button.grid(row=2, column=0, padx=20)
@@ -48,6 +52,10 @@ class QuizInterface:
         )
         self.false_button.grid(row=2, column=1, padx=20)
 
-
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text= q_text)
